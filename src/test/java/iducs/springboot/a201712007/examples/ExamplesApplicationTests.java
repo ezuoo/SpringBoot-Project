7d278e76481792a7e6e74a557eeddf15be944161
ezuoo <user@IN302>
2021-03-22 15:10:11 +0900

    first commit

---
+++ src/test/java/iducs/springboot/a201712007/examples/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package iducs.springboot.a201712007.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
