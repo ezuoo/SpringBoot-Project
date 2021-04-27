b8a6fba0873b07df980a9eb52c6bbc4c5db6f4ff
ezuoo <juho0428@naver.com>
2021-04-27 13:08:58 +0900

    configure database

--- src/main/java/iducs/springboot/a201712007/AppConfig.java
+++ src/main/java/iducs/springboot/a201712007/AppConfig.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
-import java.sql.DriverManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
-
+    @Bean(name="dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        dataSource.setDriverClassName();
+    }
 }
